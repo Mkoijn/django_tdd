cf4801eff8c40cde8c9973b2e261d97c9d50f6f8
Paul Durack <pdurack@gmail.com>
2017-10-06 04:14:31 +0100

    Helper function for checking rows

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Tom goes to check out a new online to-do app.
 		self.browser.get('http://localhost:8000')
@@ -36,23 +41,20 @@ class NewVisitorTest(unittest.TestCase):
 		# 1: "Buy socks" as an item in to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy socks', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy socks')
 
 		# Text box remains asking for another item. He enters
 		# "Put socks on"
 		# There is still a text box inviting her to add another item. He
     	# enters "Put socks on"
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys('Put socks on')
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
     	# The page updates again, and now shows both items on her list
-		
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Put socks on' ,[row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy socks')
+		self.check_for_row_in_list_table('2: Put socks on')
 
 		# Tom wonders whether the site will remember her list. He sees
 		# that the site has generated a unique URL for him -- there is some
