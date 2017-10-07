e4594bf44c13b8eb1362502fbc0cb9eacb8f962e
Paul Durack <pdurack@gmail.com>
2017-10-07 02:06:38 +0100

    refactor to use wait method instead of sleeps()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Tom goes to check out a new online to-do app.
@@ -41,8 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# When he hits enter, the page updates, and the page lists
 		# 1: "Buy socks" as an item in to-do list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy socks')
+		self.wait_for_row_in_list_table('1: Buy socks')
 
 		# Text box remains asking for another item. He enters
 		# "Put socks on"
@@ -51,11 +60,10 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Put socks on')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
     	# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('1: Buy socks')
-		self.check_for_row_in_list_table('2: Put socks on')
+		self.wait_for_row_in_list_table('1: Buy socks')
+		self.wait_for_row_in_list_table('2: Put socks on')
 
 		# Tom wonders whether the site will remember her list. He sees
 		# that the site has generated a unique URL for him -- there is some
